4a83edc0cbf9e45096fede229a2ba332d53dedbc
PaulRomanov <ramosha@mail.ru>
2021-03-02 13:28:43 +0300

    feat: start task

---
+++ practice1/script.js
@@ -0,0 +1,19 @@
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+
+let personalMovieDb = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+
+let a = prompt('Один из последних просмотренных фильмов?', '');
+let b = prompt('На сколько оцените его?', '');
+let c = prompt('Один из последних просмотренных фильмов?', '');
+let d = prompt('На сколько оцените его?', '');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log(personalMovieDb);
